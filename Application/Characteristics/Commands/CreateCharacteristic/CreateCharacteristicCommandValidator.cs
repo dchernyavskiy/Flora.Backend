f8d6cea59006486eced10e9d251909b31b3d9b78
dchernyavskiy <99835818+dchernyavskiy@users.noreply.github.com>
2023-02-10 22:51:00 +0200

    added Characteristic c/q

---
+++ Application/Characteristics/Commands/CreateCharacteristic/CreateCharacteristicCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Flora.Application.Characteristics.Commands.CreateCharacteristic;
+
+public class CreateCharacteristicCommandValidator : AbstractValidator<CreateCharacteristicCommand>
+{
+    public CreateCharacteristicCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .MinimumLength(2)
+            .MaximumLength(50);
+    }   
+}
