6a649eeb3120c8c7ecb3fa097a7f7ce3b371263f
dchernyavskiy <99835818+dchernyavskiy@users.noreply.github.com>
2023-02-11 12:17:13 +0200

    added entity config. changed identity config

---
+++ Infrastructure/Persistence/Configurations/CategoryEntityConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Flora.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Flora.Infrastructure.Persistence.Configurations;
+
+public class CategoryEntityConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.HasOne(x => x.Parent)
+            .WithMany(x => x.Children)
+            .HasForeignKey(x => x.ParentId)
+            .OnDelete(DeleteBehavior.NoAction);
+    }
+}
