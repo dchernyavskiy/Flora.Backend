6049cb0c471f3918a77f1ea7eadfe923a3d7fe62
dchernyavskiy <99835818+dchernyavskiy@users.noreply.github.com>
2023-02-19 10:31:19 +0200

    update basket c/q

---
+++ Application/Baskets/Commands/ClearBasket/ClearBasketCommand.cs
@@ -0,0 +1,22 @@
+﻿using Flora.Application.Common.Interfaces;
+using MediatR;
+
+namespace Flora.Application.Baskets.Commands.ClearBasket;
+
+public record ClearBasketCommand() : IRequest;
+
+public class ClearBasketCommandHandler : IRequestHandler<ClearBasketCommand>
+{
+    private readonly IBasketService _basketService;
+
+    public ClearBasketCommandHandler(IBasketService basketService)
+    {
+        _basketService = basketService;
+    }
+
+    public Task<Unit> Handle(ClearBasketCommand request, CancellationToken cancellationToken)
+    {
+        _basketService.Clear();
+        return Task.FromResult(Unit.Value);
+    }
+}
